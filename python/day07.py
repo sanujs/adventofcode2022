6d003855a89d845ab55a97f6bc92e1d1779ecd36
Sanuj Syal <sanujsyal@gmail.com>
2023-09-11 16:49:04 -0400

    day 7

--- python/day07.py
+++ python/day07.py
@@ -1,16 +1,14 @@
-import pprint
-    
+def navigate_to_wd(filesystem, wd):
+    pointer = filesystem
+    for d in wd:
+        pointer = pointer[d]
+    return pointer
+
 def build_filesystem():
     with open("input.txt") as f:
         filesystem = {"/": {}}
         wd = []
         pointer = filesystem
-        def navigate_to_wd(filesystem):
-            pointer = filesystem
-            for d in wd:
-                pointer = pointer[d]
-            return pointer
-
         for line in f:
             if line[0] == "$":
                 match line[2:4]:
@@ -18,7 +16,7 @@ def build_filesystem():
                         directory = line[5:].strip()
                         if directory == "..":
                             wd.pop()
-                            pointer = navigate_to_wd(filesystem)
+                            pointer = navigate_to_wd(filesystem, wd)
                         else:
                             wd.append(directory)
                             pointer = pointer[directory]
@@ -35,13 +33,52 @@ def build_filesystem():
                 pointer[file_name.strip()] = int(file_size)
                 
         return filesystem
-                    
+
+def get_directory_sizes(filesystem):
+    pointer = filesystem
+    wd = []
+    directory_sizes = {}
+
+    def recursive_accumulator(pointer):
+        acc = 0
+        for key in pointer:
+            if type(pointer[key]) == type({}):
+                wd.append(key)
+                tmp = recursive_accumulator(pointer[key])
+                # handles multiple directories with the same name
+                directory_sizes['/'.join(wd)] = tmp
+                wd.pop()
+                acc += tmp
+            else:
+                acc += pointer[key]
+        return acc
+
+    recursive_accumulator(pointer)
+    return directory_sizes
 
 
-def part_one():
-    filesystem = build_filesystem()
+def part_one(directory_sizes):
+    final_sum = 0
+    for directory in directory_sizes:
+        if directory_sizes[directory] <= 100000:
+            final_sum += directory_sizes[directory]
+    return final_sum
+
+def part_two(directory_sizes):
+    TOTAL_DISK_SPACE = 70000000
+    NEEDED_SPACE = 30000000
+    unused_space = TOTAL_DISK_SPACE - directory_sizes["/"]
+    min_deleted_file_size = NEEDED_SPACE - unused_space
+    smallest_dir = directory_sizes["/"]
+    for directory in directory_sizes:
+        if directory_sizes[directory] >= min_deleted_file_size and directory_sizes[directory] < smallest_dir:
+            smallest_dir = directory_sizes[directory]
+
+    return smallest_dir
 
 
 if __name__ == "__main__":
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(part_one())
+    filesystem = build_filesystem()
+    directory_sizes = get_directory_sizes(filesystem)
+    print(part_one(directory_sizes))
+    print(part_two(directory_sizes))
