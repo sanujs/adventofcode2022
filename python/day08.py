4bb88f5296f6d12ce02e3c8830ff1424c94d91b5
Sanuj Syal <sanujsyal@gmail.com>
2023-09-12 23:39:14 -0400

    day 8 part two

--- python/day08.py
+++ python/day08.py
@@ -49,5 +49,56 @@ def vis_bottom():
 def part_one():
     return len(set(vis_left() + vis_right() + vis_top() + vis_bottom()))
 
+def safe_seek(f, seek_position):
+    try:
+        f.seek(seek_position, 0)
+        return True
+    except ValueError:
+        return False
+
+def get_scenic_score(f, cur_tree, width):
+    if not cur_tree.isdigit(): return 0
+    cur_position = f.tell()
+    offset_map = {
+        'left': -2,
+        'right': 0,
+        'top': -width,
+        'bottom': width,
+    }
+    scenic_score = 1
+    for key in offset_map:
+        f.seek(cur_position)
+        counter = 0
+        if not safe_seek(f, f.tell() + offset_map[key]):
+            f.seek(cur_position)
+            return 0
+        tree = f.read(1)
+        while tree.isdigit():
+            counter += 1
+            if int(tree) >= int(cur_tree) or not safe_seek(f, f.tell() + offset_map[key]):
+                break
+            tree = f.read(1)
+        scenic_score *= counter
+    f.seek(cur_position)
+    return scenic_score
+
+def part_two():
+    largest_scenic_score = 0
+    with open("input.txt") as f:
+        width = len(f.readline().strip())
+        print(width)
+        f.seek(0, 2)
+        eof = f.tell()
+        f.seek(0)
+        counter = 0
+        while f.tell() < eof:
+            cur_score = get_scenic_score(f, f.read(1), width)
+            counter += 1
+            if cur_score > largest_scenic_score:
+                largest_scenic_score = cur_score
+
+    return largest_scenic_score
+
 if __name__ == "__main__":
     print(part_one())
+    print(part_two())
